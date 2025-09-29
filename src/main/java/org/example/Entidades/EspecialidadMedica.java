ad811e2d8094c3a7c45624ec6212873b6c9c24f3
AlmaPonceg <almapncee@gmail.com>
2025-09-29 13:11:13 -0300

    definitivo

---
+++ src/main/java/org/example/Entidades/EspecialidadMedica.java
@@ -0,0 +1,24 @@
+package org.example.Entidades;
+
+import lombok.*;
+
+@AllArgsConstructor
+public enum EspecialidadMedica {
+    CARDIOLOGIA("Cardiología"),
+    NEUROLOGIA("Neurología"),
+    PEDIATRIA("Pediatría"),
+    TRAUMATOLOGIA("Traumatología"),
+    GINECOLOGIA("Ginecología"),
+    UROLOGIA("Urología"),
+    OFTALMOLOGIA("Oftalmología"),
+    DERMATOLOGIA("Dermatología"),
+    PSIQUIATRIA("Psiquiatria"),
+    MEDICINA_GENERAL("Medicina General"),
+    CIRUGIA_GENERAL("Cirugía General"),
+    ANESTESIOLOGIA("Anestesiologia");
+    private final String descripcion;
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+}
---
+++ src/main/java/org/example/Entidades/EstadoCita.java
@@ -0,0 +1,20 @@
+package org.example.Entidades;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@Getter
+@AllArgsConstructor
+public enum EstadoCita {
+    PROGRAMADA("Programada"),
+    EN_CURSO("En Curso"),
+    COMPLETADA("Completada"),
+    CANCELADA("Cancelada"),
+    NO_ASISTIO("No Aistio");
+    private final String descripcion;
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+}
